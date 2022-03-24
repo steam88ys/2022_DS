2b6533049f9e3ba32d1694237837d458553530a7
steam88ys <yssteam88@gmail.com>
2022-03-24 11:04:46 +0900

    초기 커밋.

---
+++ ch01/factorial.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+//int main(void)
+//{
+//	int n = 5, facto = 1;
+//	for (int i = n; i >= 1; i--) {
+//		facto *= i;
+//	}
+//	printf("%d != %d \n", n, facto);
+//
+//	return 0;
+//}
+
+int factorial(int n)
+{
+
+}
+
+int main(void)
+{
+	int n = 5;
+
+	printf("%d != %d \n", n,factorial(n));
+}
