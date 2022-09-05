811aad90e840110ad9bfe11bde3fc6145e308843
Kim yunseo <82978463+steam88ys@users.noreply.github.com>
2022-09-05 10:17:34 +0900

    퀵 정렬

---
+++ quickSort1.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+void quickSort(int a[], int left, int right) {
+	int i=left, j=right, pivot=a[left], temp;
+
+	if (left < right) {
+		while (i < j) {
+			while (i < right && a[i] <= pivot) i++;
+			while (j > left && a[j] >= pivot) j--;
+			if (i < j) {
+				temp = a[i];
+				a[i] = a[j];
+				a[j] = temp;
+			}
+		}
+		a[left] = a[j];
+		a[j] = pivot;
+
+		quickSort(a, left, j - 1);
+		quickSort(a, j + 1, right);
+	}
+}
+
+int main(void) {
+	int a[] = { 15, 7, 2, 20, 33, 17, 26, 5, 21, 30, 27, 29, 11, 10, 3 };
+	int i, n = sizeof(a) / sizeof(int);
+
+	printf("Äü Á¤·Ä Àü : ");
+	for (i = 0; i < n; i++) printf("%5d", a[i]);
+
+	quickSort(a, 0, n - 1);
+
+	printf("\nÄü Á¤·Ä ÈÄ : ");
+	for (i = 0; i < n; i++) printf("%5d", a[i]);
+	printf("\n");
+
+	return 0;
+}
